6695216071214dac79fdaf8bdb4229b752c80a08
Jakub Prach <jakub.prach76@gmail.com>
2022-06-26 11:51:49 +0200

    Tailwind Shop

--- components/Tile.js
+++ components/Tile.js
@@ -1,100 +1,106 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Tile(){
     return(
-        <div className='container'>
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+        <div className='container mx-auto flex flex-row flex-wrap gap-3 mt-10'>
+            <div class=" mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
-            <Card className='card' style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://images.pexels.com/photos/1047540/pexels-photo-1047540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                <Card.Body>
-                    <Card.Title>Farby - 100zł</Card.Title>
-                    <Card.Text>
-                    Mam do sprzedania farby w dobrym stanie
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
 
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
+
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
+
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
+
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
+
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
+
+            <div class="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
+
+                <img class="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+                <div class="p-4">
+                <small class="text-blue-400 text-xs">Automobile company</small>
+                <h1 class="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
+                <p class="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                </div>
+            </div>
         </div>
         
         
