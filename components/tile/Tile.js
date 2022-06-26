62ff6d269008a43e05666da6768011100bfad50a
Jakub Prach <jakub.prach76@gmail.com>
2022-06-26 15:36:24 +0200

    Shop better styling

--- components/tile/Tile.js
+++ components/tile/Tile.js
@@ -5,102 +5,75 @@ export default function Tile(){
         <div className='container mx-auto flex flex-row flex-wrap gap-3 mt-10'>
             <div className=" mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/346726/pexels-photo-346726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
                 <div className="p-4">
                 <small className="text-blue-400 text-xs">Mateusz Szreder</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                <h1 className="text-2xl font-medium text-slate-600 pb-2">Gitara - 500zł</h1>
+                <p className="text-sm tracking-tight font-light text-slate-700 leading-6">Używana gitara w świetnym stanie</p>
+                <svg class="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                 </div>
             </div>
 
             <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/298298/pexels-photo-298298.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
                 <div className="p-4">
                 <small className="text-blue-400 text-xs">Jakub Prach</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                <h1 className="text-2xl font-medium text-slate-600 pb-2">Aparat - 450zł</h1>
+                <p className="text-sm tracking-tight font-light text-slate-700 leading-6">Aparat idealny dla początkującego</p>
+                <svg class="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                 </div>
             </div>
 
             <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/1462618/pexels-photo-1462618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
                 <div className="p-4">
                 <small className="text-blue-400 text-xs">Marcin Pestka</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                <h1 className="text-2xl font-medium text-slate-600 pb-2">Piłka do koszykówki - 40zł</h1>
+                <p className="text-sm tracking-tight font-light text-slate-700 leading-6">Nowa piłka w rozmiarze 7</p>
+                <svg class="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                 </div>
             </div>
 
             <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/269630/pexels-photo-269630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
                 <div className="p-4">
                 <small className="text-blue-400 text-xs">Adam Prośniewski</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                <h1 className="text-2xl font-medium text-slate-600 pb-2">Zestaw do pokera - 100zł</h1>
+                <p className="text-sm tracking-tight font-light text-slate-700 leading-6">Świetny zestaw na wieczory ze znajomymi</p>
+                <svg class="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                 </div>
             </div>
 
             <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/165236/pexels-photo-165236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
                 <div className="p-4">
                 <small className="text-blue-400 text-xs">Adam Kowalski</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                <h1 className="text-2xl font-medium text-slate-600 pb-2">Deskorolka - Oddam za darmo</h1>
+                <p className="text-sm tracking-tight font-light text-slate-700 leading-6">Deskorolka w opłakanym stanie</p>
+                <svg class="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                 </div>
             </div>
 
             <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
 
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/2570139/pexels-photo-2570139.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
 
                 <div className="p-4">
                 <small className="text-blue-400 text-xs">Mateusz Szreder</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+                <h1 className="text-2xl font-medium text-slate-600 pb-2">Futbolówka do rugby - 50zł</h1>
+                <p className="text-sm tracking-tight font-light text-slate-700 leading-6">Futbolówka do rugby dla bardziej zaawansowanych graczy</p>
+                <svg class="h-8 w-8 text-blue-500"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" /></svg>
                 </div>
-            </div>
-
-            <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
-
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-
-                <div className="p-4">
-                <small className="text-blue-400 text-xs">Mateusz Szreder</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
-                </div>
-            </div>
-
-            <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
-
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-
-                <div className="p-4">
-                <small className="text-blue-400 text-xs">Mateusz Szreder</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
-                </div>
-            </div>
-
-            <div className="mb-10 mx-auto flex w-96 flex-col justify-center bg-white rounded-2xl shadow-xl shadow-slate-300/60">
-
-                <img className="aspect-video w-96 rounded-t-2xl object-cover object-center" src="https://images.pexels.com/photos/3311574/pexels-photo-3311574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-
-                <div className="p-4">
-                <small className="text-blue-400 text-xs">Mateusz Szreder</small>
-                <h1 className="text-2xl font-medium text-slate-600 pb-2">Dodge Car</h1>
-                <p className="text-sm tracking-tight font-light text-slate-400 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
-                </div>
-            </div>
+            </div>   
         </div>
         
         
